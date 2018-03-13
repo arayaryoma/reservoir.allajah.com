b1c7049fb821d111af4e828576554c7aa90a199f
Allajah <r1o.ryoma.abe@gmail.com>
2018-03-14 04:58:19 +0900

    ng new

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'reservoir-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.stylus']
+})
+export class AppComponent {
+  title = 'reservoir';
+}
