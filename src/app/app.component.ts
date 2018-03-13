72fe6c34c12cbadfb73d9f6ec1420c962dfd0144
Allajah <r1o.ryoma.abe@gmail.com>
2018-03-14 05:05:56 +0900

    Fix extension of stylus files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'reservoir-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.stylus']
+  styleUrls: ['./app.component.styl']
 })
 export class AppComponent {
   title = 'reservoir';
