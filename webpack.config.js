2a87dbaa569f61727bff42815c140425951716c5
Allajah <r1o.ryoma.abe@gmail.com>
2018-03-28 01:53:36 +0900

    Load markdown as posts

--- webpack.config.js
+++ webpack.config.js
@@ -2,31 +2,56 @@ const path = require('path');
 const fs = require('fs');
 const htmlWebpackPlugin = require('html-webpack-plugin')
 
-module.exports = {
-    entry: './src/scripts/index.js',
+const config = {
+    entry: path.resolve(__dirname, './src/scripts/index.js'),
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].bundle.js',
     },
-    plugins: [
-        new htmlWebpackPlugin({
-            template: 'src/index.html',
-            filename: 'index.html',
-        }),
-    ],
     module: {
         rules: [
             {
                 test: /\.md$/,
                 use: [
-                    { loader: 'html-loader'},
-                    { loader: 'markdown-loader' }
+                    {loader: 'html-loader'},
+                    {loader: 'markdown-loader'}
                 ]
             },
         ]
-    }
+    },
+    plugins: []
 };
 
-const getPosts = () => {
-    fs.readdirSync()
+const generatePages = (posts) => {
+    const res = [];
+    for (let post of posts) {
+        const name = /(.*)\.md$/.exec(post)[1];
+        res.push(
+            new htmlWebpackPlugin({
+                template: './src/layout/post.html',
+                title: name,
+                filename: `posts/${name}.html`,
+                post: post
+            })
+        );
+    }
+    return res;
 };
+
+
+const validatePosts = posts => {
+    const res = [];
+    for (let post of posts) {
+        if (postRegex.exec(post) !== null) {
+            res.push(post);
+        }
+    }
+    return res;
+};
+
+const postRegex = /\.md$/;
+const posts = fs.readdirSync(path.resolve('./src/posts'), {encoding: 'utf8'});
+const validPosts = validatePosts(posts);
+
+config.plugins.push(...generatePages(validPosts));
+module.exports = config;
